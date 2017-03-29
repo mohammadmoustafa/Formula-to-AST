6f0155b330f60d4c1c52349b24ca1328d9f3f3d9
Mohammad Moustafa <mohammad.m@live.ca>
2017-03-28 23:25:13 -0400

    rough version of algorithm implemented

--- shunting-yard-conversion.py
+++ shunting-yard-conversion.py
@@ -1,2 +1,26 @@
-def in_to_postfix(formula):
-    pass
+def postfix(formula):
+    output = []
+    operands = {'+':1, '*':1, '-':1}
+    brackets = ['(', ')']
+    stack = []
+    index = 0
+    while index < len(formula):
+        token = formula[index]
+        if token.isnumeric():
+            output.append(token)
+        elif token in operands:
+            stack.append(token)
+        elif token == '(':
+            stack.append(token)
+        elif token == ')':
+            while stack[-1] != '(':
+                output.append(stack.pop())
+            stack.pop()
+        index += 1
+
+    while stack != []:
+        output.append(stack.pop())
+
+    print(output)
+
+
